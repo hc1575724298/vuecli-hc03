60c34f2795331ad022d5d2e224bdbbf2c218dfcd
huchun <1575724298@qq.com>
2022-06-30 17:49:54 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: suiyue
+ * @email: 1373842098@qq.com
+ * @Date: 2022-06-30 17:44:45
+ * @LastEditors: sj
+ * @LastEditTime: 2022-06-30 17:46:06
+-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
